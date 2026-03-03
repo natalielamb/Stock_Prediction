541a7dbacc7ca8da3f327892c405fa8209e1136f
natalielamb <natalie.lamb@tcu.edu>
2026-03-03 13:34:54 -0600

    Update model pipeline filename and comments
    Updated model pipeline filename in MODEL_INFO and adjusted comments for clarity.

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -58,7 +58,7 @@ df_features = extract_features()
 MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
-        "pipeline": 'finalized_model.tar.gz',
+        "pipeline": 'finalized_pair_model.tar.gz',
         "keys": ["MPWR", "AAPL"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
 }
@@ -160,6 +160,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
