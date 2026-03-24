558f069d244190a137f30fd6356731a3c4c4234d
natalielamb <natalie.lamb@tcu.edu>
2026-03-24 11:29:23 -0700

    Add files via upload

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -14,46 +14,62 @@ import sys
 
 # ... continue with your script ...
 
-def extract_features_pair():
+def extract_features():
 
     return_period = 5
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['GOOGL', 'CBRE'] #['BTC-USD', 'ETH-USD', 'SPY', 'GLD']
-    #ccy_tickers = ['DTWEXBGS']
-    #idx_tickers = ['SP500', 'VIXCLS']
+    stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+    idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
     #stk_data = web.DataReader(stk_tickers, 'yahoo')
-    #ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
-    #idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
+    ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
+    idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
 
-    #Y = np.log(stk_data.loc[:, ('Adj Close', 'BTC-USD')]).diff(return_period).shift(-return_period)
-    #Y.name = Y.name[-1]+'_Future'
+    Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+    Y.name = Y.name[-1]+'_Future'
+    
+    X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+    X1.columns = X1.columns.droplevel()
+    X2 = np.log(ccy_data).diff(return_period)
+    X3 = np.log(idx_data).diff(return_period)
+
+    X = pd.concat([X1, X2, X3], axis=1)
+    
+    dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
+    Y = dataset.loc[:, Y.name]
+    X = dataset.loc[:, X.columns]
+    dataset.index.name = 'Date'
+    #dataset.to_csv(r"./test_data.csv")
+    features = dataset.sort_index()
+    features = features.reset_index(drop=True)
+    features = features.iloc[:,1:]
+    return features
+
+def extract_features_pair():
+
+    START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+    END_DATE = datetime.date.today().strftime("%Y-%m-%d")
+    stk_tickers = ['AAPL', 'MPWR']
+    
+    stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
+
     Y = stk_data.loc[:, ('Adj Close', 'AAPL')]
     Y.name = 'AAPL'
 
     X = stk_data.loc[:, ('Adj Close', 'MPWR')]
     X.name = 'MPWR'
-    
-    #X1 = np.log(stk_data.loc[:, ('Adj Close', ('ETH-USD', 'SPY', 'GLD'))]).diff(return_period)
-    #X1.columns = X1.columns.droplevel()
-    #X2 = np.log(ccy_data).diff(return_period)
-    #X3 = np.log(idx_data).diff(return_period)
 
-    #X = pd.concat([X1, X2, X3], axis=1)
-    
-    dataset = pd.concat([Y, X], axis=1).dropna()#.iloc[::return_period, :]
+    dataset = pd.concat([Y, X], axis=1).dropna()
     Y = dataset.loc[:, Y.name]
     X = dataset.loc[:, X.name]
     dataset.index.name = 'Date'
-    #dataset.to_csv(r"./test_data.csv")
     features = dataset.sort_index()
     features = features.reset_index(drop=True)
-    #features = features.iloc[:,1:]
     return features
-
 
 def get_bitcoin_historical_prices(days = 60):
     
@@ -71,8 +87,4 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
-
-
-
-
 
