626f72b8ca6f26e33a258397a3f2865e632564c3
natalielamb <natalie.lamb@tcu.edu>
2026-04-07 12:54:25 -0500

    Refactor Streamlit app for improved functionality

--- Portfolio/StreamlitApp_HW5_Part_2.py
+++ Portfolio/StreamlitApp_HW5_Part_2.py
@@ -57,8 +57,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pca.shap', 
         "pipeline": 'finalized_pca_model.tar.gz', 
-        "keys": ["RSI_15","MOM_15"], 
-        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["RSI_15","MOM_15"]] 
+        "keys": ["RSI_10","MA_15"], 
+        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["RSI_10","MA_15"]] 
 }
 
 def load_pipeline(_session, bucket, key):
